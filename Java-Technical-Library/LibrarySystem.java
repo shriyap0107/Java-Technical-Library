26226da4fa17b4f10fee790866227f98704de9a3
Shriya Patel <shriyap0107@gmail.com>
2026-03-29 23:53:53 +0530

    Add files via upload

---
+++ Java-Technical-Library/LibrarySystem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+// This is a Class representing a Research Paper (OOP Concept: Encapsulation)
+class ResearchPaper {
+    String title;
+    String author;
+    String field;
+
+    public ResearchPaper(String title, String author, String field) {
+        this.title = title;
+        this.author = author;
+        this.field = field;
+    }
+}
+
+public class LibrarySystem {
+    public static void main(String[] args) {
+        System.out.println("--- Technical Resource Manager Starting ---");
+        // TODO: Implement menu for adding and viewing papers
+        // TODO: Implement File Saving logic
+    }
+}
